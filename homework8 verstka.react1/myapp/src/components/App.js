6c2db2d7381c0c754b1e86313e32a489c9a05a8c
AlinaKobetss <145448396+AlinaKobetss@users.noreply.github.com>
2024-05-14 16:20:49 +0200

    Add files via upload

---
+++ homework8 verstka.react1/myapp/src/components/App.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Container from "./Container/Container";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Proposition from "../components/Proposition";
+import Feedback from "./Feedback";
+import Bikerental from "./Bikerental";
+import Services from "./Services";
+
+function App() {
+
+let services = [
+  {
+    border: "1px solid #ffffff",
+    title: "Годовое ТО",
+    backgroundColor: "#22356F"
+  },
+  {
+    border: "1px solid #ffffff",
+    title: "Выравнивание колес",
+    backgroundColor: "#0052C1"
+  }
+  , 
+  {
+    border: "1px solid #ffffff",
+    title: "Настройка переключателей",
+    backgroundColor: "#76B58B"
+  }
+  
+
+]
+
+
+  return (
+    <div>
+      <Container backgroundColor="#F4F9E2">
+        <Header />
+        <Hero />
+      </Container>
+      <Container backgroundColor="#62D0DF">
+        <Proposition />
+      </Container>
+      <Container>
+        <Feedback />
+        <div style={{ display: "flex" }}>
+          
+      </div>
+      {services.map((service) => (
+      <Services key={service.id} title={service.title} border={service} backgroundColor= { service.backgroundColor}/>
+    ))}
+
+        <Bikerental />
+      </Container>
+    
+
+   
+    </div>
+  );
+}
+
+export default App;
+
+// https://docs.google.com/document/d/1x4yBLVfaWkt92uItbDxe4DUBRSuUf2ZWEpXRUxBrbMM/edit
---
+++ homework8 verstka.react1/myapp/src/components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = (props) => {
+  const { title, backgroundColor } = props;
+  return (
+    <div className='services' style={{ backgroundColor }}>
+      <div className='services-content'>
+        <h3>{title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
